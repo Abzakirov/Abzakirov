8af9d5e73072062a5b0922c49625dd990f539024
AbdullohZokirov <abdullazokirov808@gmail.com>
2024-11-18 14:44:59 +0500

    14__dars tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    colors: {
+      1: " #ff9846",
+      2: "#f2f2f2",
+      3: "#111",
+      4: "#a4acad",
+      5: "#f46d40",
+      7: "#f2f2f2",
+      8: " #fff",
+      9: "#A4ACAD",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+  
+};
